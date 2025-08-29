7fb718a6eabbcf75e4a58a93db05641a87deb990
amantiwari59821-source <amantiwari59821@gmail.com>
2025-08-29 22:59:54 +0530

    Updated songs list

--- music.py
+++ music.py
@@ -5,15 +5,21 @@ import random
 songs = {
     "happy": [
         "https://youtu.be/kw4tT7SCmaY?feature=shared",
-        "https://youtu.be/X4RFNTTUizY?feature=shared"
+        "https://youtu.be/X4RFNTTUizY?feature=shared",
+        "https://youtu.be/d7AqPH-LgmI?feature=shared",,
+        "https://youtu.be/eM8Mjuq4MwQ?feature=shared"
     ],
     "sad": [
         "https://youtu.be/kp-Bqr1Gtyw?feature=shared",
-        "https://youtu.be/x-wCUOYxYH4?feature=shared"
+        "https://youtu.be/x-wCUOYxYH4?feature=shared",
+        "https://youtu.be/7iyBBAfd_X8?feature=shared",
+        "https://youtu.be/AX6OrbgS8lI?feature=shared"
     ],
     "study": [
        "https://youtu.be/GiVxUKbIy0w?feature=shared",
-       "https://youtu.be/nHvkz0dPHNM?feature=shared"
+       "https://youtu.be/nHvkz0dPHNM?feature=shared",
+       "https://youtu.be/vB0V3iCSzQw?feature=shared",
+       "https://youtu.be/tYKrORILFOg?feature=shared"
     ]
 }
 
